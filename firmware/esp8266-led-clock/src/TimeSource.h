1085396403b5d8abadf3b4dc8fa96a65f5565431
Andriy Malyshenko <amalyshenko@klika-tech.com>
2024-02-25 22:41:45 +0100

    Fix for failing auto time discovery

--- firmware/esp8266-led-clock/src/TimeSource.h
+++ firmware/esp8266-led-clock/src/TimeSource.h
@@ -16,6 +16,8 @@
 #include <WiFiManager.h>
 #include <WiFiClientSecure.h>
 #include <Ticker.h>
+#include <ArduinoJson.h>
+#include <EEPROM.h>
 #include <time.h>
 #include "tz.h"
 #include "renderer/Loader.h"
@@ -26,8 +28,14 @@
 #define NTP_UPDATE_EVERY_SEC (60 * 60 * 2) // Every 2 hrs
 
 const String _my_ip_url = "https://api.my-ip.io/ip";
-const String _my_timezone_url = "http://ip-api.com/line/?fields=timezone";
-const String _my_timeoffset_url = "http://ip-api.com/line/?fields=offset";
+const String _my_json_url = "http://ip-api.com/json?fields=status,offset";
+
+#define EEPROM_MAGIC_NUMBER 0x12
+
+struct SavedSettings
+{
+    uint32_t tz_offset;
+};
 
 class TimeSource
 {
@@ -36,6 +44,7 @@ private:
     WiFiManager *_wifiManager;
     Ticker tick;
     tm Time;
+    SavedSettings settings;
 
     WiFiClientSecure _secureclient;
     WiFiClient _client;
@@ -105,6 +114,7 @@ void TimeSource::disconnect()
 void TimeSource::init()
 {
     tick.attach(NTP_UPDATE_EVERY_SEC, __timesource_tick_callback, this);
+    EEPROM.begin(1 + sizeof(SavedSettings));
 }
 
 bool TimeSource::getUrl(const String url, String &result)
@@ -118,43 +128,44 @@ bool TimeSource::getUrl(const String url, String &result)
 
     HTTPClient http;
 
-    if (http.begin(client, url))
-    { // HTTP
+    for (uint tries = 0; tries < 3; tries++)
+    {
+        if (http.begin(client, url))
+        { // HTTP
 
-        Serial.printf("[HTTP] GET %s\n", url.c_str());
-        // start connection and send HTTP header
-        int httpCode = http.GET();
+            Serial.printf("[HTTP] GET %s\n", url.c_str());
+            // start connection and send HTTP header
+            int httpCode = http.GET();
 
-        // httpCode will be negative on error
-        if (httpCode > 0)
-        {
-            // HTTP header has been send and Server response header has been handled
-            Serial.printf("[HTTP] GET %d code\n", httpCode);
-
-            // file found at server
-            if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY)
+            // httpCode will be negative on error
+            if (httpCode > 0)
             {
-                String payload = http.getString();
-                // Serial.printf("[HTTP] GET %s\n", payload.c_str());
-                result = payload;
+                // HTTP header has been send and Server response header has been handled
+                Serial.printf("[HTTP] GET %d code\n", httpCode);
+
+                // file found at server
+                if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY)
+                {
+                    String payload = http.getString();
+                    // Serial.printf("[HTTP] GET %s\n", payload.c_str());
+                    result = payload;
+                }
+                else
+                {
+                    return false;
+                }
             }
             else
             {
-                return false;
+                Serial.printf("[HTTP] (%d of %d) GET failed, error: %s\n", tries + 1, 3, http.errorToString(httpCode).c_str());
             }
+
+            http.end();
         }
         else
         {
-            Serial.printf("[HTTP] GET failed, error: %s\n", http.errorToString(httpCode).c_str());
-            return false;
+            Serial.printf("[HTTP] (%d of %d) Unable to connect\n", tries + 1, 3);
         }
-
-        http.end();
-    }
-    else
-    {
-        Serial.printf("[HTTP} Unable to connect\n");
-        return false;
     }
 
     return true;
@@ -165,20 +176,42 @@ bool TimeSource::sync()
     if (!connect())
         return false;
 
-    // _loader->setState(GET_IP);
-    // String ip = "";
-    // if (getUrl(_my_ip_url, ip))
-    // {
-    //     Serial.println("Public IP Address: " + ip);
-    // }
-
     _loader->setState(GET_TZ);
-    String tz = "0";
-    if (getUrl(_my_timeoffset_url, tz))
+
+    String response = "";
+    if (getUrl(_my_json_url, response))
     {
-        Serial.printf("Timezone offset: %s\n", tz);
+        Serial.printf("Response: %s\n", response.c_str());
+        DynamicJsonDocument doc(256);
+        deserializeJson(doc, response);
+        const char *status = doc["status"];
+        if (String(status) == "success")
+        {
+            settings.tz_offset = doc["offset"];
+            EEPROM.put(0, EEPROM_MAGIC_NUMBER);
+            EEPROM.put(1, settings);
+            EEPROM.commit();
+        }
+        else
+        {
+            Serial.print("Failed to get TZ offset, try restoring from saved state\n");
+            if (EEPROM.read(0) == EEPROM_MAGIC_NUMBER)
+                EEPROM.get(1, settings);
+            else
+                Serial.print("No state is saved in EEPROM, falling back to UTC\n");
+        }
     }
-    configTime(atoi(tz.c_str()), 0, NTP_SERVERS);
+    else
+    {
+        Serial.print("Failed to get TZ offset, restoring from saved state\n");
+        if (EEPROM.read(0) == EEPROM_MAGIC_NUMBER)
+            EEPROM.get(1, settings);
+        else
+            Serial.print("No state is saved in EEPROM, falling back to UTC\n");
+    }
+
+    Serial.printf("Timezone offset: %d\n", settings.tz_offset);
+    configTime(settings.tz_offset, 0, NTP_SERVERS);
 
     _loader->setState(GET_TIME);
     Serial.print("Requesting current time...");
